06db707df3f5e3c3ca98b3c9c8f68a6db26a8799
timeojavaux <timeo.javaux@iut-dijon.u-bourgogne.fr>
2026-04-06 14:20:44 +0200

    Skills section

---
+++ js/nav_select.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll('section, header');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        // On détecte si le scroll est au niveau de la section (avec une marge de 100px)
+        if (pageYOffset >= (sectionTop - 100)) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(current)) {
+            link.classList.add('active');
+        }
+    });
+});
